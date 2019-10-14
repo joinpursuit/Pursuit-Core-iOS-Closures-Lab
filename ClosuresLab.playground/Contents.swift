cc4da842dda3e82b7354b02a1fb787f0e4e78d3c
Bienbenido Angeles <bangeles@Bienbenidos-MacBook-Pro.local>
2019-10-14 17:04:50 -0400

    completed all lab questions except for number 4

--- ClosuresLab.playground/Contents.swift
+++ ClosuresLab.playground/Contents.swift
@@ -7,13 +7,19 @@ import UIKit
 
 // Your function here
 
+func applyKTimes(_ k: Int, closure: () -> ()){
+    for _ in 1...k {
+        closure()
+    }
+}
+
 // Uncomment out the following lines to check your solution
 
-//var myVal = 0
-//applyKTimes(5) {
-//    myVal += 1
-//}
-//assert(myVal == 5, "Expected myVal to be five, but was \(myVal)")
+var myVal = 0
+applyKTimes(5) {
+    myVal += 1
+}
+assert(myVal == 5, "Expected myVal to be five, but was \(myVal)")
 
 
 // Question Two
@@ -22,12 +28,18 @@ import UIKit
 
 // Your function here
 
+func multiples(of number: Int, in arr: [Int]) -> [Int] {
+    arr.filter{(num) -> Bool in
+        return num % number == 0
+    }
+}
+
 // Uncomment out the following lines to check your solution
 
-//let numbers = [1, 2, 3, 4, 6, 8, 9, 3, 12, 11]
-//let expectedOutputTwo = [3, 6, 9, 3, 12]
-//let outputTwo = multiples(of: 3, in: numbers)
-//assert(outputTwo == expectedOutputTwo, "Expected output to be \(expectedOutputTwo), but found \(outputTwo)")
+let numbers = [1, 2, 3, 4, 6, 8, 9, 3, 12, 11]
+let expectedOutputTwo = [3, 6, 9, 3, 12]
+let outputTwo = multiples(of: 3, in: numbers)
+assert(outputTwo == expectedOutputTwo, "Expected output to be \(expectedOutputTwo), but found \(outputTwo)")
 
 
 // Question Three
@@ -36,12 +48,26 @@ import UIKit
 
 // Your function here
 
+func largestValue(in arr: [Int]) -> Int {
+    
+    
+    guard let first = numbers.first else {return -1}
+    let result = arr.reduce(first) { previousResult, currentValue in
+        if previousResult > currentValue {
+            return previousResult
+        } else {
+            return currentValue
+        }
+    }
+    return result
+}
+
 // Uncomment out the following lines to check your solution
 
-//let moreNumbers = [4, 7, 1, 9, 6, 5, 6, 9]
-//let expectedOutputThree = 9
-//let outputThree = largestValue(in: moreNumbers)
-//assert(outputThree == expectedOutputThree, "Expected output to be \(expectedOutputThree), but found \(outputThree)")
+let moreNumbers = [4, 7, 1, 9, 6, 5, 6, 9]
+let expectedOutputThree = 9
+let outputThree = largestValue(in: moreNumbers)
+assert(outputThree == expectedOutputThree, "Expected output to be \(expectedOutputThree), but found \(outputThree)")
 
 
 // Question Four
@@ -49,6 +75,10 @@ import UIKit
 // Write a function called sortedNamesByLastName(in:) that takes in an array of tuples of type (String, String) and returns an array of tuples sorted by last name.
 
 // Your function here
+
+func sortedNamesByLastName(in tuplesArr:[(String, String)]) -> [(String, String)] {
+    tuplesArr.sorted(by: {})
+}
 
 // Uncomment out the following lines to check your solution
 
@@ -80,6 +110,12 @@ import UIKit
 // Write a function called sumOfSquaresOfOddNumbers(in:) that returns the sum of the squares of all the odd numbers from an array of Ints.  Use filter, map and reduce in your function.
 
 // Your function here
+
+func sumOfSquaresOfOddNumbers(in arr:[Int]) -> Int {
+    let filter = arr.filter{(num) -> Bool in num % 2 == 1}
+    let map = filter.map{($0 * $0)}
+    let reduce = map.reduce{}
+}
 
 // Uncomment out the following lines to check your solution
 
